153b8137a527147828c75954b8eb653f0ab2e490
Стас Басов <stas.basov@whatever.com>
2023-01-30 16:18:50 +0700

    pered review

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+now = date.today()
+now_year = now.year
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    return {
+        'year': now_year
+    }
---
+++ yatube/posts/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Group , User
+from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
+from .forms import PostForm
+
+
+def index(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    context = { 'page_obj' : page_obj}
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group)[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'page_obj':page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def group_list(request,slug):
+    posts = Post.objects.all()
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group':group,
+        'page_obj':page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'author':author,
+    'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post.objects.select_related('author','group'),id=post_id)
+    post_list = post.author.posts.all()
+    context = {'post':post,
+    'post_list':post_list,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', request.user)
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+def post_edit(request,post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    context = {
+        'form': form,
+        'is_edit': True,
+        'post': post,
+    }
+    if post.author == request.user:
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.save()
+            return redirect('posts:post_detail', post_id)
+        return render(request, 'posts/update_post.html',context)
+    return redirect('posts:post_detail', post_id)
+
+
+class JustStaticPage(TemplateView):
+    template_name = 'app_name/just_page.html'
+
+class AboutAuthorView(TemplateView):
+    template_name = 'app_name/author.html'
+
+class AboutTechView(TemplateView):
+    template_name = 'app_name/tech.html'
