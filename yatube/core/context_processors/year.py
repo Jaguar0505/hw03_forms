0e3fd749581c41df27d9ac023f42f386148b0054
Стас Басов <stas.basov@whatever.com>
2023-02-01 15:28:02 +0700

    Review 3.0

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -1,11 +1,8 @@
 from datetime import date
 
-now = date.today()
-now_year = now.year
-
 
 def year(request):
     """Добавляет переменную с текущим годом."""
     return {
-        'year': now_year
+        'year': date.today().year
     }
